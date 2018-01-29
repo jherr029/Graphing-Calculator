a65e711435e7bb3944af023d0483b6667fc5f771
makloooo <mlieu002@ucr.edu>
2018-01-29 05:13:24 -0800

    Minimal working example of login

--- app/src/main/java/teamteam/graphing_calculator/MainActivity.java
+++ app/src/main/java/teamteam/graphing_calculator/MainActivity.java
@@ -1,13 +1,27 @@
 package teamteam.graphing_calculator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button sign_in = findViewById(R.id.gotoauth);
+        sign_in.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int targetId = view.getId();
+        if (targetId == R.id.gotoauth) {
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        }
     }
 }
