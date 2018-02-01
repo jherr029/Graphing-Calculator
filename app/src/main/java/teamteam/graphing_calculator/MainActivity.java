cdfc812b0ef611bcc42b501e5c893f7ea73a508e
makloooo <mlieu002@ucr.edu>
2018-02-01 05:22:31 -0800

    Swept auth into LoginModule class

--- app/src/main/java/teamteam/graphing_calculator/MainActivity.java
+++ app/src/main/java/teamteam/graphing_calculator/MainActivity.java
@@ -16,72 +16,36 @@ import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    GoogleSignInClient mGoogleSignInClient;
-    GoogleSignInAccount userAccount;
-    int RC_SIGN_IN = 779;
-    private static final String TAG = MainActivity.class.getSimpleName();
+    LoginModule mLoginModule;
+
+    MainActivity() { mLoginModule = new LoginModule(this); }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Configures options to request when signing in.
-        // .requestIdToken("617175976667-ljedvh5ceofngqtmu18qt9e2gobgfn6h.apps.googleusercontent.com")
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail()
-                .build();
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        mLoginModule.onCreate();
 
         findViewById(R.id.sign_in_button).setOnClickListener(this);
-
     }
 
     @Override
     protected void onStart() {
         super.onStart();
 
-        // Returns null if no account exists
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-        updateUI(account);
+        mLoginModule.onStart();
     }
 
     @Override
     public void onClick(View view) {
         int id = view.getId();
-
-        if (id == R.id.sign_in_button) signIn();
+        if (id == R.id.sign_in_button) mLoginModule.signIn();
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
-        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            handleSignInResult(task);
-        }
-    }
-
-    private void updateUI(GoogleSignInAccount user) {
-        userAccount = user;
-        if (user == null) Toast.makeText(getApplicationContext(), "Account is null", Toast.LENGTH_SHORT).show();
-        else Toast.makeText(getApplicationContext(), user.getEmail(), Toast.LENGTH_SHORT).show();
-    }
-
-    private void signIn() {
-        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
-
-    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
-        try {
-            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-            updateUI(account);
-        } catch (ApiException e) {
-            Log.w(TAG, "signInResult:failed code="+ e.getStatusCode());
-            updateUI(null);
-        }
+        mLoginModule.onActivityResult(requestCode, data);
     }
 }
