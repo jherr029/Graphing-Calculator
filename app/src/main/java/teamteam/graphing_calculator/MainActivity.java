55cb9860405bcfabbf252101a212bbbc3c709173
makloooo <mlieu002@ucr.edu>
2018-02-01 13:35:19 -0800

    Revert "Merge branch 'Mark'"
    This reverts commit 1b0e59c0d44f07836f7c8e00efa2f505a948cb7b, reversing
    changes made to 8e36512b2f5196180c6f0b814fd1665828b1964a.

--- app/src/main/java/teamteam/graphing_calculator/MainActivity.java
+++ app/src/main/java/teamteam/graphing_calculator/MainActivity.java
@@ -1,42 +1,13 @@
 package teamteam.graphing_calculator;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-    LoginModule mLoginModule;
-
-    MainActivity() { mLoginModule = new LoginModule(this); }
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mLoginModule.onCreate();
-
-        findViewById(R.id.sign_in_button).setOnClickListener(this);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        mLoginModule.onStart();
-    }
-
-    @Override
-    public void onClick(View view) {
-        int id = view.getId();
-        if (id == R.id.sign_in_button) mLoginModule.signIn();
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        mLoginModule.onActivityResult(requestCode, data);
     }
 }
