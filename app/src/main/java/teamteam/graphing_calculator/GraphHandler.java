11bcc9f9b41f1e19c92bf5dbde4b4e403d3aee47
avana002 <avana002@ucr.edu>
2018-03-05 13:46:22 -0800

    Finished runtime line adjustment

--- app/src/main/java/teamteam/graphing_calculator/GraphHandler.java
+++ app/src/main/java/teamteam/graphing_calculator/GraphHandler.java
@@ -2,11 +2,16 @@ package teamteam.graphing_calculator;
 
 import android.app.Activity;
 import android.renderscript.ScriptGroup;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.EditText;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.DataPointInterface;
 import com.jjoe64.graphview.series.LineGraphSeries;
+import com.jjoe64.graphview.series.OnDataPointTapListener;
+import com.jjoe64.graphview.series.Series;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -21,6 +26,10 @@ public class GraphHandler {
     private GraphView graph;
     private ExpressionEvaluation parser;
     private Map functions;
+    private double[] touchedpt = new double[]{Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY};
+    private String touchedse = "";
+    private double sens = 50;
+    private int inc = 90;
 
     public int min_x = -30;
     public int max_x = 30;
@@ -28,10 +37,81 @@ public class GraphHandler {
     public int max_y = 45;
     private String gtype = "Cartesian";
 
+    private int lbound = 80;
+    private int rbound = 1055;
+    private int tbound = 20;
+    private int bbound = 1670;
+
+    //converts a pixel position to a position on the graph.
+    //assumes position is within the bounds of the graph.
+    private double[] getgraphpos (double[] loc){
+        double[] pos = new double[2];
+
+        double min_x = graph.getViewport().getMinX(false);
+        double max_x = graph.getViewport().getMaxX(false);
+        double min_y = graph.getViewport().getMinY(false);
+        double max_y = graph.getViewport().getMaxY(false);
+
+        //x val
+        pos[0] = min_x + ((loc[0] - lbound)/(rbound - lbound)) * (max_x - min_x);
+        //y val
+        pos[1] = max_y - ((loc[1] - tbound)/(bbound - tbound)) * (max_y - min_y);
+        return pos;
+    }
+
+    //converts a position on the graph to a pixel position
+    private double[] getpixelpos (double[] loc){
+        double[] pos = new double[2];
+
+        double min_x = graph.getViewport().getMinX(false);
+        double max_x = graph.getViewport().getMaxX(false);
+        double min_y = graph.getViewport().getMinY(false);
+        double max_y = graph.getViewport().getMaxY(false);
+
+        pos[0] = (rbound - lbound) * ((loc[0] - min_x)/(max_x - min_x)) + lbound;
+        pos[1] = (bbound - tbound) * ((loc[1] - min_y)/(max_y - min_y)) + tbound;
+
+        return pos;
+    }
+
+    //evaluates distance between two points
+    private double distance(double[] a, double[] b){
+        return Math.sqrt(Math.pow(b[0]-a[0],2) + Math.pow(b[1]-a[1],2));
+    }
+
+    //returns the closest graphed point to the point touched and the series it is in.
+    private String getclosest(double[] touch, double[] point){
+        double dist = Double.POSITIVE_INFINITY;
+        String series = "";
+        //iterate through list of lines
+        Iterator it = functions.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry)it.next();
+            LineGraphSeries<DataPoint> s = (LineGraphSeries<DataPoint>)pair.getValue();
+            //iterate through points on the line
+            Iterator it1 = s.getValues(s.getLowestValueX(),s.getHighestValueX());
+            while(it1.hasNext()){
+                DataPoint dp = (DataPoint)it1.next();
+                double[] pt = new double[]{dp.getX(),dp.getY()};
+                //compute distance between touched point and the datapoint
+                double d = distance(touch,pt);
+                //if the datapoint is the closest so far
+                if(d < dist){
+                    //update shortest distance, closest point, and the series
+                    dist = d;
+                    point[0] = pt[0];
+                    point[1] = pt[1];
+                    series = (String)pair.getKey();
+                }
+            }
+        }
+        return series;
+    }
+
     public GraphHandler (Activity act) {
 
-        this.mainact = act;
-        graph = this.mainact.findViewById(R.id.graph);
+        mainact = act;
+        graph = mainact.findViewById(R.id.graph);
         functions = new HashMap();
         parser = new ExpressionEvaluation();
 
@@ -47,13 +127,101 @@ public class GraphHandler {
         // enable scaling and scrolling
         graph.getViewport().setScalable(true);
         graph.getViewport().setScalableY(true);
+
+        graph.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent ev) {
+                //position of touched point in terms of pixels
+                double[] touchpx = new double[]{ev.getX(),ev.getY()};
+                //if point is not on the graph, it is ignored
+                if(touchpx[0] < lbound || touchpx[0] > rbound || touchpx[1] < tbound || touchpx[1] > bbound){
+                    graph.getViewport().setScrollable(true);
+                    graph.getViewport().setScrollableY(true);
+                    touchedpt = new double[]{Double.POSITIVE_INFINITY,Double.POSITIVE_INFINITY};
+                    touchedse = "";
+                    return false;
+                }
+                //position of touched point in terms of graph coordinates
+                double[] touchgr = getgraphpos(touchpx);
+
+                switch(ev.getAction()){
+                    //user begins a touch
+                    case MotionEvent.ACTION_DOWN: {
+                        if(!functions.isEmpty()){
+                            //closest datapoint to the touched point in terms of graph coordinates
+                            double[] ptgr = new double[2];
+                            //series containing closest datapoint
+                            touchedse = getclosest(touchgr,ptgr);
+                            //if series found
+                            if(!touchedse.equals("")){
+                                //you have to convert it back from touchgr for it to work.
+                                //I dont know why.
+                                touchpx = getpixelpos(touchgr);
+                                //closest datapoint to the touched point in terms of pixels
+                                double[] ptpx = getpixelpos(ptgr);
+                                //if touched point is within sensitivity range of datapoint
+                                if(distance(ptpx,touchpx) < sens){
+                                    //System.out.println("Point touched");
+                                    //turn off scrolling
+                                    graph.getViewport().setScrollable(false);
+                                    graph.getViewport().setScrollableY(false);
+                                    touchedpt = ptgr;
+                                }
+                            }
+                        }
+                        break;
+                    }
+                    //user releases a touch
+                    case MotionEvent.ACTION_UP: {
+                        //turn scrolling back on
+                        graph.getViewport().setScrollable(true);
+                        graph.getViewport().setScrollableY(true);
+                        //if a datapoint was touched previously
+                        if(touchedpt[0] != Double.POSITIVE_INFINITY) {
+                            //change in x
+                            double deltax = touchgr[0] - touchedpt[0];
+                            //change in y
+                            double deltay = touchgr[1] - touchedpt[1];
+                            //updated datapoints
+                            DataPoint[] newdata = new DataPoint[inc];
+                            //iterate through touched series
+                            int i = 0;
+                            LineGraphSeries s = (LineGraphSeries) functions.get(touchedse);
+                            Iterator it = s.getValues(s.getLowestValueX(),s.getHighestValueX());
+                            while(it.hasNext()){
+                                DataPoint dp = (DataPoint)it.next();
+                                //create updated datapoints
+                                newdata[i] = new DataPoint(dp.getX()+deltax,dp.getY()+deltay);
+                                i++;
+                            }
+                            //apply new datapoints
+                            ((LineGraphSeries) functions.get(touchedse)).resetData(newdata);
+                            touchedpt = new double[]{Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY};
+                            touchedse = "";
+                        }
+                        break;
+                    }
+                    //touch fails
+                    case MotionEvent.ACTION_CANCEL: {
+                        graph.getViewport().setScrollable(true);
+                        graph.getViewport().setScrollableY(true);
+                        touchedpt = new double[]{Double.POSITIVE_INFINITY,Double.POSITIVE_INFINITY};
+                        touchedse = "";
+                        break;
+                    }
+                }
+
+                return false;
+            }
+        });
     }
 
     private DataPoint[] gen_data (String func){
 
         //generates points from the function
-        int inc = 90;
         DataPoint[] points = new DataPoint[inc];
+        func = func.replaceAll("pi",Double.toString(Math.PI));
+        func = func.replaceAll("e",Double.toString(Math.E));
 
         if(gtype.equals("Cartesian")) {
             double step = (double) Math.abs(max_x - min_x) / inc;
